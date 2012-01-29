936794ddb61d7d8071bcefa6534f61253c1355fa
muel96 <muel96@4233c665-1772-a6e2-a4fe-a7c3e4e1d018>
2012-01-29 02:38:06 +0000

    Quick update. no real changes.

--- trunk/mw2ffex/mw2ffex.h
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <stdint.h>
-#include <string.h>
-#include <ctype.h>
-#include <sign_ext.h>
-#include <direct.h>
-
-int extract_fastfile(char * infilename, char * outfilename);
